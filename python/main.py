d53bcb68a3b488391aae0f9c1256cd293a59e294
RobPedrosa29 <rob.pedrosa99@gmail.com>
2025-09-09 21:56:53 -0300

    Funções iniciais de parcela_existente, explica_parcela, area_cana_acucar e estruturação inicial do calculo_area_cana

--- python/main.py
+++ python/main.py
@@ -25,3 +25,32 @@ def parcela_cana_acucar():
 def criar_parcela():
     novo_id = "P" + str(len(parcelas) + 1)
 
+def parcela_existente():
+    pass
+
+def explica_parcela():
+    print("Descrição de uma parcela")
+
+def area_cana_acucar():
+    area = input("""Selecione uma das opções abaixo: 
+        1- Ja possuo o valor da área;
+        2- Não possuo o valor da área;
+        
+        """).strip().lower()
+    
+    match area:
+        case "1" | "um" | "ja possuo":
+            pass   ### if m² ou ha
+        case "2" | "dois" | "não possuo" | "nao possuo":
+            pass   #### chamar função para calcular área
+
+def calculo_area_cana():
+    while True:
+        try:
+            comprimento = float(input("Digite o valor do comprimento, exemplo: 5000"))
+            largura = float(input("Digite o valor da largura, exemplo: 2000"))
+            break
+        except ValueError:
+            print("Por gentileza, digite apenas valores numéricos.")
+    result_area = comprimento * largura
+    return result_area
